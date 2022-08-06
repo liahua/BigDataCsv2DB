4863a3a241c0d155479b3e57c4211c2db2641c5e
liahua <85178516@qq.com>
2022-08-06 17:31:13 +0800

    leetcode 14-1

--- src/main/java/com/chenahua/transform/TransformApplication.java
+++ src/main/java/com/chenahua/transform/TransformApplication.java
@@ -2,7 +2,9 @@ package com.chenahua.transform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
+@Order
 @SpringBootApplication
 public class TransformApplication {
 
--- src/test/java/com/chenahua/transform/TransformApplicationTests.java
+++ src/test/java/com/chenahua/transform/TransformApplicationTests.java
@@ -4,8 +4,11 @@ import com.chenahua.transform.service.TransformService;
 import com.chenahua.transform.service.impl.ReadTask;
 import com.google.common.primitives.Longs;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.AnnotationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -16,7 +19,7 @@ import java.util.Random;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+@EnableAspectJAutoProxy
 @SpringBootTest
 class TransformApplicationTests {
 
@@ -26,7 +29,9 @@ class TransformApplicationTests {
     @Test
     void contextLoads() throws SQLException, IOException, InterruptedException, ExecutionException {
 //        ArrayList<ReadTask> readTasks = transformService.providerMulti();
-//        System.out.println(readTasks);
+//        System.out.pr0intln(readTasks);
+
+
         int bbc=333333;
         transformService.transformCSV(true);
         ForkJoinPool forkJoinPool = new ForkJoinPool(5);
@@ -52,4 +57,5 @@ class TransformApplicationTests {
 
         System.out.println("longs = " + longs);
     }
+
 }
